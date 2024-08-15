8e5f6619c95ba6633c5e5545712eece28ed670c2
Siren Kumar Padhy <padhysiren93@hotmaill.com>
2024-08-15 17:37:50 +0530

    Defect 12345 fixed null value to stored in database

---
+++ src/main/java/com/git_examples/B.java
@@ -0,0 +1,8 @@
+package com.git_examples;
+
+public class B {
+
+    public void test (){
+        System.out.println("test");
+    }
+}
---
+++ src/main/java/com/git_examples/C.java
@@ -0,0 +1,8 @@
+package com.git_examples;
+
+public class C {
+
+    public void test1(){
+        System.out.println("test1");
+    }
+}
